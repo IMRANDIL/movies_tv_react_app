9069a81baa496c4a33e7b8c7a7846a4307af94e7
IMRANDIL <aliimranadil2@gmail.com>
2022-01-30 18:34:48 +0530

    yh

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { Container } from '@mui/material'
 import './App.css';
 import Header from './components/Header/Header';
 import SimpleBottomNavigation from './components/MainNav';
+import Trending from './pages/Trending/Trending';
+import Search from './pages/Search/Search';
+import Movies from './pages/Movies/Movies';
+import Series from './pages/Series/Series';
+
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <div className="app">
+        <Container>
+          <Routes>
+            <Route path='/' element={<Trending />} exact />
+            <Route path='/movies' element={<Movies />} />
+            <Route path='/series' element={<Series />} />
+            <Route path='/search' element={<Search />} />
 
+
+          </Routes>
+        </Container>
       </div>
+
       <SimpleBottomNavigation />
     </BrowserRouter>
   );
---
+++ src/pages/Series/Series.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Series() {
+    return <div>
+        <span className='pageTitle'>Search</span>
+    </div>;
+}
+
+export default Series;
