78c7023ee7a0ba036d15db1802d94887e35d2650
IMRANDIL <aliimranadil2@gmail.com>
2022-01-31 11:52:26 +0530

    yh

---
+++ src/components/singleContent/SingleContent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './SingleContent.css';
+function SingleContent() {
+    return <div>
+        hello
+    </div>;
+}
+
+export default SingleContent;
--- src/pages/Trending/Trending.js
+++ src/pages/Trending/Trending.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import SingleContent from '../../components/singleContent/SingleContent';
 
 function Trending() {
+
+
+    const [content, setContent] = useState([])
+
+
+    const fetchTrending = async () => {
+        const { data } = await axios.get(`
+        https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}`);
+
+        setContent(data.results)
+    }
+
+
+
+    useEffect(() => {
+        fetchTrending()
+    }, [])
+
+
+
+
+
+
+
+
     return <div>
         <span className='pageTitle'>Trending</span>
+        <div className='trending'>
+            {
+                content && content.map(({ id, poster_path, title, name, first_air_date, release_date, media_type, vote_average }) => {
+                    return (
+                        <SingleContent key={id} id={id} poster={poster_path} title={title || name} date={first_air_date || release_date} media_type={media_type} vote_average={vote_average} />
+                    )
+                })
+            }
+        </div>
     </div>;
 }
 
