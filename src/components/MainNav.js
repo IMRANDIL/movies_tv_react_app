758a89eff7d159f86790049cf33cfb3ff4619950
IMRANDIL <aliimranadil2@gmail.com>
2022-01-31 11:04:43 +0530

    yh

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -6,6 +6,7 @@ import WhatshotIcon from '@mui/icons-material/Whatshot';
 import MovieIcon from '@mui/icons-material/Movie';
 import SearchIcon from '@mui/icons-material/Search';
 import TvIcon from '@mui/icons-material/Tv';
+import { useNavigate } from 'react-router';
 
 //styles..
 
@@ -15,6 +16,22 @@ import TvIcon from '@mui/icons-material/Tv';
 export default function SimpleBottomNavigation() {
 
     const [value, setValue] = React.useState(0);
+    const history = useNavigate();
+
+    React.useEffect(() => {
+        if (value === 0) {
+            history('/')
+        }
+        else if (value === 1) {
+            history('/movies')
+        }
+        else if (value === 2) {
+            history('/series')
+        }
+        else {
+            history('/search')
+        }
+    }, [value, history])
 
     return (
         <Box style={{
--- src/pages/Series/Series.js
+++ src/pages/Series/Series.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Series() {
     return <div>
-        <span className='pageTitle'>Search</span>
+        <span className='pageTitle'>Searies</span>
     </div>;
 }
 
