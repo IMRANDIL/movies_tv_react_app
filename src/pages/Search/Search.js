fcbacf9e8dd56dd8d22a80ca40496079eca96a9a
IMRANDIL <aliimranadil2@gmail.com>
2022-02-01 10:11:48 +0530

    yh

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,9 +1,79 @@
-import React from 'react';
+import { Button, Tab, Tabs, TextField } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import Search from '@mui/icons-material/Search';
+import axios from 'axios';
+import SingleContent from '../../components/singleContent/SingleContent';
+import CustomPagination from '../../components/CustomPagination/CustomPagination';
 
-function Search() {
+
+function Seaarch() {
+
+    const [type, setType] = useState(0);
+    const [page, setPage] = useState(1);
+    const [searchText, setSearchText] = useState('');
+    const [content, setContent] = useState();
+    const [numOfPages, setNumOfPages] = useState();
+
+
+
+    const fetchSearch = async () => {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${process.env.REACT_APP_API_KEY
+            }&language=en-US&query=${searchText}&page=${page}&include_adult=false`);
+
+
+        setContent(data.results);
+        setNumOfPages(data.total_pages)
+    }
+
+
+    useEffect(() => {
+
+        window.scroll(0, 0);
+        fetchSearch()
+        // eslint-disable-next-line
+    }, [type, page])
+
+
+
     return <div>
-        <span className='pageTitle'>Search</span>
+        <div style={{ display: 'flex', margin: '15px 0' }}>
+            <TextField
+                style={{ flex: 1, backgroundColor: '#2d313a' }}
+                className='searchbox'
+                label='Search'
+                variant='filled'
+                onChange={(e) => setSearchText(e.target.value)}
+
+
+            />
+            <Button variant='contained' style={{ marginLeft: 10 }} onClick={fetchSearch}><Search /></Button>
+        </div>
+        <Tabs value={type} indicatorColor='primary' textColor='primary' onChange={(event, newValue) => {
+            setType(newValue);
+            setPage(1)
+        }} centered style={{ paddingBottom: 5 }}>
+
+            <Tab label="Search Movies" style={{ width: '50%', color: 'white' }} />
+            <Tab label="Search Tv Series" style={{ width: '50%', color: 'white' }} />
+
+        </Tabs>
+
+
+        <div className='trending'>
+            {
+                content && content.map(({ id, poster_path, title, name, first_air_date, release_date, vote_average }) => {
+                    return (
+                        <SingleContent key={id} id={id} poster={poster_path} title={title || name} date={first_air_date || release_date} media_type={type ? 'tv' : 'movie'} vote_average={vote_average} />
+                    )
+                })
+            }
+            {searchText && !content && (type ? <h2>No Series Found</h2> : <h2>No Movies Found</h2>)}
+        </div>
+        {numOfPages > 1 && (
+            <CustomPagination setPage={setPage} numOfPages={numOfPages} />)}
+
+
     </div>;
 }
 
-export default Search;
+export default Seaarch;
--- src/pages/Series/Series.js
+++ src/pages/Series/Series.js
@@ -1,8 +1,66 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import CustomPagination from '../../components/CustomPagination/CustomPagination';
+import Genres from '../../components/Genres';
+import SingleContent from '../../components/singleContent/SingleContent'
+import useGenres from '../../Hooks/useGenre';
 
 function Series() {
+
+
+    const [page, setPage] = useState(1);
+    const [content, setContent] = useState([]);
+    const [numOfPages, setNumOfPages] = useState();
+    const [selectedGenres, setSelectedGenres] = useState([]);
+    const [genres, setGenres] = useState([]);
+    const genreforURL = useGenres(selectedGenres)
+
+    const fetchMovies = async () => {
+        const { data } = await axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`);
+        setContent(data.results);
+        setNumOfPages(data.total_pages)
+    }
+
+
+    useEffect(() => {
+        fetchMovies()
+        // eslint-disable-next-line
+    }, [page, genreforURL])
+
+
+
+
+
+
+
+
+
     return <div>
         <span className='pageTitle'>Searies</span>
+
+        <Genres
+            type='tv'
+            selectedGenres={selectedGenres}
+            genres={genres}
+            setGenres={setGenres}
+            setSelectedGenres={setSelectedGenres}
+            setPage={setPage}
+        />
+
+
+
+
+        <div className='trending'>
+            {
+                content && content.map(({ id, poster_path, title, name, first_air_date, release_date, vote_average }) => {
+                    return (
+                        <SingleContent key={id} id={id} poster={poster_path} title={title || name} date={first_air_date || release_date} media_type='tv' vote_average={vote_average} />
+                    )
+                })
+            }
+        </div>
+        {numOfPages > 1 && (
+            <CustomPagination setPage={setPage} numOfPages={numOfPages} />)}
     </div>;
 }
 
