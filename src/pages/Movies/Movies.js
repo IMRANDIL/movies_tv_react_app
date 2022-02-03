d9c2ac4ba0a66ad1ba00e83926bdd7a274c823fd
ALI IMRAN ADIL <aliimranadil2@gmail.com>
2022-02-03 19:20:13 +0530

    Update Movies.js

--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -18,7 +18,7 @@ function Movies() {
     const fetchMovies = async () => {
         const { data } = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`);
         setContent(data.results);
-        setNumOfPages(500)
+        setNumOfPages(data.total_pages)
     }
 
 
